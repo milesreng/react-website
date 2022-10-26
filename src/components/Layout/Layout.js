b20e81955c53dfa815e251be53b677397b973c2c
milesreng <miles.eng@duke.edu>
2022-10-26 13:33:46 -0400

    testing navbar2 class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from "../Navbar/NavBar";
+import navbar2 from '../Navbar/navbar2';
 
 import '../App.scss';
 
@@ -8,6 +9,7 @@ class Layout extends Component {
     render() {
         return (
             <div classname="App">
+                <navbar2 />
                 <NavBar />
                 <div className="page">
                     <span className="tags top-tags h2">public static void main(String args, int[] argv*) &#123;</span>
---
+++ src/components/Navbar/navbar2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+class navbar2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <Link to="/about">to about</Link>
+            </div>
+        );
+    }
+}
+
+export default navbar2;
